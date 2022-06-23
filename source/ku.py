8d7ea80a5a8f30c8b133f1289aa98a10fb81a32b
Shane <shanechou@gmail.com>
2022-06-23 16:30:41 +0800

    Fixed 推送錯誤

--- source/ku.py
+++ source/ku.py
@@ -153,19 +153,19 @@ class KUCrawler:
                     if self.connection == None or self.channel == None:
                         self.connection, self.channel = init_session(self._config['rabbitmqUrl'])
 
-                    elif self.connection.is_closed or self.channel.is_closed or not _upload_status:
+                    elif self.connection.is_closed or self.channel.is_closed or not self._upload_status:
                         if connection.is_open:
                             connection.close()
                         self.connection, self.channel = init_session(self._config['rabbitmqUrl'])
 
                     self._upload_status = upload_data(self.channel, protobufData, gameType)
-                    self._logger.debug(_upload_status)
+                    self._logger.debug(self._upload_status)
 
                 except Exception:
                     traceback.print_exc()
                     self._logger.error("Can't connect to MQ.")
                 else:
-                    self._logger.info("Send MQ status : " + str(_upload_status))
+                    self._logger.info("Send MQ status : " + str(self._upload_status))
             else :
                 self._logger.info("Data is empty." )        
 
