e2c69088b15c78354a8d07e83976c7fd89d4823b
shane <shane@seektopser.com>
2022-07-02 15:45:50 +0800

    修正 連線程式

--- source/ku.py
+++ source/ku.py
@@ -135,6 +135,7 @@ class KUCrawler:
             for task in self._tasks:
                 if str(task['game_type']) in crawlList and str(task['game_mode']) in crawlModeList:
                     webSocketList = task['socket']
+                    needConnect = False
                     sportType = 1
                     #檢查是否有抓到menu，不用管運動總類，只要確認盤口("早盤"、"今日"、"走地")。
                     #如沒抓到就開啟預設webcoket(type = 1)，抓“全場”
@@ -163,27 +164,35 @@ class KUCrawler:
 
                     typeKey = str(task['game_type']) + "_" + str(task['game_mode']) + "_" + str(sportType)    
 
-                    for index, url in enumerate(self._url):
-                        reConnect = False
+                    if not typeKey in webSocketList:
+                        webSocketList[typeKey] = {}
+                        needConnect = True
+                    else : 
                         for typeIndex in webSocketList:
-                            if typeIndex in webSocketList and url in webSocketList[typeIndex] and webSocketList[typeIndex][url]['socket'].isClose():
-                                typeKey = typeIndex
-                                sportType = int(typeIndex[-1])
-                                reConnect = True
-                                break
-                            else :
-                                reConnect = False
+                            if typeIndex in webSocketList:
+                                urlList = webSocketList[typeIndex]
+                                for url in urlList:
+                                    if urlList[url]['socket'].isClose():
+                                        typeKey = typeIndex
+                                        sportType = int(typeIndex[-1])
+                                        needConnect = True
+                                    else :
+                                        needConnect = False
+                                        break
 
-                        if not typeKey in webSocketList or reConnect:
-                            socket = KuWebSocket(url, self._urlSearch, self._protocol, on_open=self.on_open, on_message=self.on_message, on_keepLive=self.on_keepLive, crawlIndex=crawlList[task['game_type']], crawlMode=crawlModeList[task['game_mode']], crawlType=str(sportType))
-                            startThread = threading.Thread(target = socket.connect)
-                            if not typeKey in webSocketList:
-                                webSocketList[typeKey] = {}
-                            typeItem = webSocketList[typeKey]
-                            typeItem[url] = {
-                                'socket' : socket
-                            }
-                            startThread.start()
+                                if needConnect: 
+                                    break
+
+                    if needConnect:
+                        for index, url in enumerate(self._url):
+                            if not url in webSocketList[typeKey] or needConnect:
+                                socket = KuWebSocket(url, self._urlSearch, self._protocol, on_open=self.on_open, on_message=self.on_message, on_keepLive=self.on_keepLive, crawlIndex=crawlList[task['game_type']], crawlMode=crawlModeList[task['game_mode']], crawlType=str(sportType))
+                                startThread = threading.Thread(target = socket.connect)     
+                                typeItem = webSocketList[typeKey]
+                                typeItem[url] = {
+                                    'socket' : socket
+                                }
+                                startThread.start()
 
                 else:
                     self._logger.debug(f'Not support sport[{task["game_type"]}] mode[{task["game_mode"]}]')
