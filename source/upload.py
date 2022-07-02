21e65197e9318d6d1787fc5a62691dc96ad4a1e6
shane <shane@seektopser.com>
2022-07-02 13:57:35 +0800

    預防半全場無賠率

--- source/upload.py
+++ source/upload.py
@@ -150,7 +150,17 @@ class protobufUtils:
 
     #半全場
     def setFullHalfOutcome(eventBuf, oddsItem):
-        eventBuf.multi = "{\"HH\": " + str(oddsItem[Mapping.oddsData.hf_HH]) + ", \"HD\": " + str(oddsItem[Mapping.oddsData.hf_HD]) + ", \"HA\": " + str(oddsItem[Mapping.oddsData.hf_HA]) + ", \"DH\": " + str(oddsItem[Mapping.oddsData.hf_DH]) + ", \"DD\": " + str(oddsItem[Mapping.oddsData.hf_DD]) + ", \"DA\": " + str(oddsItem[Mapping.oddsData.hf_DA]) + ", \"AH\": " + str(oddsItem[Mapping.oddsData.hf_AH]) + ", \"AD\": " + str(oddsItem[Mapping.oddsData.hf_AD]) + ", \"AA\": " + str(oddsItem[Mapping.oddsData.hf_AA]) + "}"    
+        hl_HH = "0.0" if len(str(oddsItem[Mapping.oddsData.hf_HH])) == 0 else str(oddsItem[Mapping.oddsData.hf_HH])
+        hl_HD = "0.0" if len(str(oddsItem[Mapping.oddsData.hf_HD])) == 0 else str(oddsItem[Mapping.oddsData.hf_HD])
+        hl_HA = "0.0" if len(str(oddsItem[Mapping.oddsData.hf_HA])) == 0 else str(oddsItem[Mapping.oddsData.hf_HA])
+        hl_DH = "0.0" if len(str(oddsItem[Mapping.oddsData.hf_DH])) == 0 else str(oddsItem[Mapping.oddsData.hf_DH])
+        hl_DD = "0.0" if len(str(oddsItem[Mapping.oddsData.hf_DD])) == 0 else str(oddsItem[Mapping.oddsData.hf_DD])
+        hl_DA = "0.0" if len(str(oddsItem[Mapping.oddsData.hf_DA])) == 0 else str(oddsItem[Mapping.oddsData.hf_DA])
+        hl_AH = "0.0" if len(str(oddsItem[Mapping.oddsData.hf_AH])) == 0 else str(oddsItem[Mapping.oddsData.hf_AH])
+        hl_AD = "0.0" if len(str(oddsItem[Mapping.oddsData.hf_AD])) == 0 else str(oddsItem[Mapping.oddsData.hf_AD])
+        hl_AA = "0.0" if len(str(oddsItem[Mapping.oddsData.hf_AA])) == 0 else str(oddsItem[Mapping.oddsData.hf_AA])
+
+        eventBuf.multi = "{\"HH\": " + hl_HH + ", \"HD\": " + hl_HD + ", \"HA\": " + hl_HA + ", \"DH\": " + hl_DH + ", \"DD\": " + hl_DD + ", \"DA\": " + hl_DA + ", \"AH\": " + hl_AH + ", \"AD\": " + hl_AD + ", \"AA\": " + hl_AA + "}"    
 
         return eventBuf
 
